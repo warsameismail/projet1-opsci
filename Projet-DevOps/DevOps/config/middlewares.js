19995f49ba2092f3a387af910397aad9458a9062
warsame <warsame.ismail@etu.sorbonne-universite.fr>
2025-04-17 17:59:39 +0200

    add front + back

---
+++ Projet-DevOps/DevOps/config/middlewares.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { name: 'strapi::logger', config: {} },
+  { name: 'strapi::errors', config: {} },
+  { name: 'strapi::security', config: {} },
+  { name: 'strapi::cors', config: {} },
+  { name: 'strapi::poweredBy', config: {} },
+  { name: 'strapi::query', config: {} },
+  { name: 'strapi::body', config: {} },
+  { name: 'strapi::session', config: {} },
+  { name: 'strapi::favicon', config: {} },
+  { name: 'strapi::public', config: {} },
+];
